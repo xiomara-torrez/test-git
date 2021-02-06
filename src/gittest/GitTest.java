353f40be60d21a4a940b0725fb9c2762b36569fe
Xiomara Torrez Muñoz <000329744@MED.UPB>
2021-02-06 12:24:46 -0500

    Creación del proyecto en el repositorio -000329744

---
+++ src/gittest/GitTest.java
@@ -0,0 +1,16 @@
+package gittest;
+
+/**
+ *
+ * @author 000329744 - Xiomara Torrez Muñoz
+ */
+public class GitTest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello Wordl");
+    }
+    
+}

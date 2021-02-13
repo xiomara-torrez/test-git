34be55b3a654c9dbc685561d9d5792b7aaa54b24
Xiomi_Torrez <xioitorres86@gmail.com>
2021-02-12 21:06:07 -0500

    Se colocó Xiomy

--- src/gittest/GitTest.java
+++ src/gittest/GitTest.java
@@ -10,7 +10,7 @@ public class GitTest {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hello Wordl - Modificado");
+        System.out.println("Hello Wordl - Modificado - Xiomy");
     }
     
 }
